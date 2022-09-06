157a50bca5d04f02ad30f064111408fa95c0b3b2
J-Puente <85426932+J-Puente@users.noreply.github.com>
2022-09-06 08:11:00 -0500

    Resubmission

---
+++ app.js
@@ -0,0 +1,16 @@
+const A = "Name" ; //Variables declared outside of a function have Global Scope
+
+/*This is how you block notes*/
+
+{var b = "What is my name?"; } //Block variables 
+
+const firstName = 'Janeth';
+
+const lastName = 'Cruz';
+
+
+console.log(firstName + ' ' + lastName);
+
+alert('Hello! My name is:')
+
+
